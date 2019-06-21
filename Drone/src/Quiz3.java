1002a3ffff62bb555d6295ae36baa712bef49ed1
JeongJongHwan <ghks602@kunsan.ac.kr>
2019-06-21 10:52:58 +0900

    Quiz3

--- Drone/src/Quiz3.java
+++ Drone/src/Quiz3.java
@@ -1,4 +1,15 @@
-
 public class Quiz3 {
-
+    public static void main(String[] args){
+        int num = 13195;
+ 
+        while(num!=1){
+            for(int i=2; i<=num; i++){
+                if(num%i==0){
+                    num /= i;
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
+    }
 }
