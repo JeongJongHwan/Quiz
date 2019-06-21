78d426f96997850a642693a60eeb991d6332420c
JeongJongHwan <ghks602@kunsan.ac.kr>
2019-06-21 10:23:58 +0900

    Answer Code for Quiz

---
+++ Drone/src/Quiz2.java
@@ -0,0 +1,22 @@
+public class Quiz2 {
+ 
+    public static void main(String[] args) {
+        
+    	int num = 55;
+        int a = 1;
+        int b = 1;
+        int fibo = 0;
+        int sum = 0;
+        
+        for(int i=1; i<=num; i++){
+            fibo=a+b;
+            System.out.println(fibo);
+            a=b;
+            b=fibo;
+            if(fibo%2==0) {
+            	sum = sum + fibo;
+            }
+        }
+        System.out.println(sum);
+    }
+}
